50e1aa466665d374699cea02b094cdf94709abb6
Daniel Schwartz <SCHDAN037@users.noreply.github.com>
2017-09-08 19:51:40 +0200

    git is annoying me
    updating contact things

--- MentorWebApp/MentorWebApp/Models/ApplicationUser.cs
+++ MentorWebApp/MentorWebApp/Models/ApplicationUser.cs
@@ -9,5 +9,9 @@ namespace MentorWebApp.Models
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        public string name { get; set; }
+        public string email { get; set; }
+        public string role { get; set; }
+
     }
 }
