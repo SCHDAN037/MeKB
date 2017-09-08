96f8ff40bc2a1051be807f92a53e81a222b7485f
Daniel Schwartz <SCHDAN037@users.noreply.github.com>
2017-09-08 17:27:16 +0200

    New Project
    Add New Project Using .NET Core 2.0 MVC Web app

---
+++ MentorWebApp/MentorWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MentorWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
